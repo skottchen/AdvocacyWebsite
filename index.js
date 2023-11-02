9ecfc6ebf3eed11411f7e63f034302bea27ba556
Scott Chen <96890985+skottchen@users.noreply.github.com>
2023-11-02 13:31:43 -0400

    Added form functionality, completed Unit 6 Project

--- index.js
+++ index.js
@@ -9,6 +9,46 @@ const toggleDarkMode = () => {
 
 themeButton.addEventListener("click", toggleDarkMode);
 
+// Add your query for the sign now button here
+const form = document.getElementById("sign-petition")
+let count = 3;
 
-// TODO: Register a 'click' event listener for the theme button
-// Set toggleDarkMode as the callback function.
+const addSignature = (event) => {
+  event.preventDefault();
+
+  let name = document.getElementById('name').value;
+  let hometown = document.getElementById('hometown').value;
+  // Add signature to the list
+  const signed = document.querySelector(".signatures");
+  const newSig = document.createElement("p");
+  newSig.textContent = "🖊️ " + name + " from " + hometown + " supports this.";
+  signed.appendChild(newSig);
+
+  // Update the signature count
+  count = count + 1;
+
+  // Remove the old counter
+  const oldCounter = document.getElementById("counter");
+  if (oldCounter) {
+    oldCounter.remove();
+  }
+
+  // Create a new counter element
+  const counterElement = document.createElement("p");
+  counterElement.id = "counter";
+  counterElement.textContent = "🖊️ " + count + " people have signed this petition and support this cause.";
+
+  // Append the new counter to the signatures div
+  signed.appendChild(counterElement);
+
+  // Clear input fields
+  document.getElementById('name').value = '';
+  document.getElementById('hometown').value = '';
+}
+
+
+// Add a click event listener to the sign now button here
+form.addEventListener('submit', addSignature);
+
+// functionality to start keeping a count of total signatures
+
